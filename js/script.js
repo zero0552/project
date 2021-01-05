2bab80df13f87e3cf77fd523f4fd6023eec32c87
Dmitriy <kotenkoKaiser@yandex.ru>
2021-01-05 21:43:46 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const isCheched = false,
+      isClose = false;
+    
+console.log(isCheched || isClose);
